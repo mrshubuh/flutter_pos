c0b8e6b17b35a1234af2d9f9630467e762565904
mrshubuh <163012212+mrshubuh@users.noreply.github.com>
2026-03-15 20:45:08 +0700

    Fix typo in DropdownButtonFormField value property

--- lib/presentation/widgets/app_drop_down.dart
+++ lib/presentation/widgets/app_drop_down.dart
@@ -147,7 +147,7 @@ class _SingleSelectDropDown<T> extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DropdownButtonFormField<T>(
-      initialValue: dropdown.selectedValue,
+      Value: dropdown.selectedValue,
       onChanged: dropdown.onChanged,
       items: dropdown.dropdownItems,
       style: dropdown.textStyle(context),
